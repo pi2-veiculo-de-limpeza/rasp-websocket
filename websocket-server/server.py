3acb9c0fe5d878d3e8243637fac3c2b36443b342
Lucas Costa <lucascostaa73@gmail.com>
2018-11-29 14:36:14 -0200

    Setting websocket server for python2

--- websocket-server/server.py
+++ websocket-server/server.py
@@ -1,25 +1,26 @@
-import asyncio
-import websockets
-import time
+from websocket_server import WebsocketServer
 
-async def hello(websocket, path):
-    while True:
-        try:
-            name = await websocket.recv()
-            print("< " + name)
+# Called for every client connecting (after handshake)
+def new_client(client, server):
+	print("New client connected and was given id %d" % client['id'])
+	server.send_message_to_all("Hey all, a new client has joined us")
 
-            greeting = "Server: " + name
 
-            await websocket.send(greeting)
-            print("> " + greeting)
-            
-            time.sleep(0.001)
-        except websockets.exceptions.ConnectionClosed:
-            print("end connection :D")
-            break
-    
+# Called for every client disconnecting
+def client_left(client, server):
+	print("Client(%d) disconnected" % client['id'])
 
-start_server = websockets.serve(hello, '0.0.0.0', 8000)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+# Called when a client sends a message
+def message_received(client, server, message):
+	if len(message) > 200:
+		message = message[:200]+'..'
+	print("Client(%d) said: %s" % (client['id'], message))
+
+
+PORT=8000
+server = WebsocketServer(PORT, host='0.0.0.0')
+server.set_fn_new_client(new_client)
+server.set_fn_client_left(client_left)
+server.set_fn_message_received(message_received)
+server.run_forever()
